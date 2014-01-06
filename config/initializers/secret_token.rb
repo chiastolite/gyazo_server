8289ec4eb74c29ee5b8572bb3919a7dfcd1c791b
chiastolite <chiastolite.1980@gmail.com>
2014-01-06 23:17:35 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GyazoServer::Application.config.secret_key_base = '45ab45c1d716acc1e801fcfe0c6aa5e5ad8c4214b9267541b3e6107aa43aa3734bbe9ceb071afd09e342abf786fecd1268df79dcfe8ec78e0958c482667ef921'
