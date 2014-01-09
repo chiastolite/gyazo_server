64756bc6549d1e775edb1db20da28aebd4fbb2c2
chiastolite <chiastolite.1980@gmail.com>
2014-01-10 07:22:11 +0900

    user_id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def user_id
+    params['id'] || new_id
+  end
+
+  def new_id
+    Digest::MD5.hexdigest(request[:remote_addr] + Time.now.to_s)
+  end
 end
