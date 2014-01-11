50305e280d6d1c189ebb50e89eb209969f19ffd9
chiastolite <chiastolite.1980@gmail.com>
2014-01-11 10:53:47 +0900

    [from now] 2014/01/11 10:53:47
    diff --git a/db/migrate/20140110163828_add_title_to_screenshots.rb b/db/migrate/20140110163828_add_title_to_screenshots.rb
    new file mode 100644
    index 0000000..2be80cd
    --- /dev/null
    +++ b/db/migrate/20140110163828_add_title_to_screenshots.rb
    @@ -0,0 +1,5 @@
    +class AddTitleToScreenshots < ActiveRecord::Migration
    +  def change
    +    add_column :screenshots, :title, :string
    +  end
    +end
    diff --git a/db/schema.rb b/db/schema.rb
    index d1ad1ee..9439133 100644
    --- a/db/schema.rb
    +++ b/db/schema.rb
    @@ -11,7 +11,7 @@
     #
     # It's strongly recommended that you check this file into your version control system.
    
    -ActiveRecord::Schema.define(version: 20140106153652) do
    +ActiveRecord::Schema.define(version: 20140110163828) do
    
       create_table "screenshots", force: true do |t|
         t.datetime "created_at"
    @@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20140106153652) do
         t.string   "image_content_type"
         t.integer  "image_file_size"
         t.datetime "image_updated_at"
    +    t.string   "title"
       end
    
     end

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140106153652) do
+ActiveRecord::Schema.define(version: 20140110163828) do
 
   create_table "screenshots", force: true do |t|
     t.datetime "created_at"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20140106153652) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.string   "title"
   end
 
 end
