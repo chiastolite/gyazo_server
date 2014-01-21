2f428af70b42f8124f324bbd7236633c1327eb6f
chiastolite <chiastolite.1980@gmail.com>
2014-01-21 18:19:54 +0900

    [from now] 2014/01/21 18:19:54
    diff --git a/app/controllers/screenshots_controller.rb b/app/controllers/screenshots_controller.rb
    index 256f5ce..e303abe 100644
    --- a/app/controllers/screenshots_controller.rb
    +++ b/app/controllers/screenshots_controller.rb
    @@ -2,46 +2,27 @@ class ScreenshotsController < ApplicationController
       before_action :set_screenshot, only: [:show, :destroy]
       skip_before_action :verify_authenticity_token, only: [:create]
    
    -  # GET /screenshots
    -  # GET /screenshots.json
       def index
    -    logger.info('test')
         @screenshots = Screenshot.all
       end
    
    -  # GET /screenshots/1
    -  # GET /screenshots/1.json
       def show
       end
    
    -  # POST /screenshots
    -  # POST /screenshots.json
       def create
    -    logger.info(params)
         @screenshot = Screenshot.new
         @screenshot.image = params[:imagedata]
         @screenshot.save
         render text: screenshot_url(@screenshot)
       end
    
    -  # DELETE /screenshots/1
    -  # DELETE /screenshots/1.json
       def destroy
         @screenshot.destroy
    -    respond_to do |format|
    -      format.html { redirect_to screenshots_url }
    -      format.json { head :no_content }
    -    end
    +    redirect_to screenshots_url
       end
    
       private
    -    # Use callbacks to share common setup or constraints between actions.
    -    def set_screenshot
    -      @screenshot = Screenshot.find(params[:id])
    -    end
    -
    -    # Never trust parameters from the scary internet, only allow the white list through.
    -    #def screenshot_params
    -    #  params[:screenshot]
    -    #end
    +  def set_screenshot
    +    @screenshot = Screenshot.friendly.find(params[:id])
    +  end
     end
    diff --git a/app/models/screenshot.rb b/app/models/screenshot.rb
    index 8ef0719..43dc60d 100644
    --- a/app/models/screenshot.rb
    +++ b/app/models/screenshot.rb
    @@ -1,7 +1,11 @@
     class Screenshot < ActiveRecord::Base
       extend FriendlyId
       has_attached_file :image, styles: { thumb: '100x100>' }, default_url: '/images/:style/missing.png',
    -    path: ':rails_root/public/sys_img/:id_sha1/:style.png',
    -    url:  "#{ActionController::Base.relative_url_root}/sys_img/:id_sha1/:style.png"
    -  friendly_id :image, use: :slugged
    +    path: ':rails_root/public/screenshots/:style/:id_sha1.png',
    +    url:  "#{ActionController::Base.relative_url_root}/screenshots/:style/:id_sha1.png"
    +  friendly_id :image_sha1, use: :slugged
    +
    +  def image_sha1
    +    File.basename(image.url[/(.*)\?.*/, 1], '.png')
    +  end
     end

--- app/controllers/screenshots_controller.rb
+++ app/controllers/screenshots_controller.rb
@@ -2,46 +2,27 @@ class ScreenshotsController < ApplicationController
   before_action :set_screenshot, only: [:show, :destroy]
   skip_before_action :verify_authenticity_token, only: [:create]
 
-  # GET /screenshots
-  # GET /screenshots.json
   def index
-    logger.info('test')
     @screenshots = Screenshot.all
   end
 
-  # GET /screenshots/1
-  # GET /screenshots/1.json
   def show
   end
 
-  # POST /screenshots
-  # POST /screenshots.json
   def create
-    logger.info(params)
     @screenshot = Screenshot.new
     @screenshot.image = params[:imagedata]
     @screenshot.save
     render text: screenshot_url(@screenshot)
   end
 
-  # DELETE /screenshots/1
-  # DELETE /screenshots/1.json
   def destroy
     @screenshot.destroy
-    respond_to do |format|
-      format.html { redirect_to screenshots_url }
-      format.json { head :no_content }
-    end
+    redirect_to screenshots_url
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_screenshot
-      @screenshot = Screenshot.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    #def screenshot_params
-    #  params[:screenshot]
-    #end
+  def set_screenshot
+    @screenshot = Screenshot.friendly.find(params[:id])
+  end
 end
--- app/models/screenshot.rb
+++ app/models/screenshot.rb
@@ -1,7 +1,11 @@
 class Screenshot < ActiveRecord::Base
   extend FriendlyId
   has_attached_file :image, styles: { thumb: '100x100>' }, default_url: '/images/:style/missing.png',
-    path: ':rails_root/public/sys_img/:id_sha1/:style.png',
-    url:  "#{ActionController::Base.relative_url_root}/sys_img/:id_sha1/:style.png"
-  friendly_id :image, use: :slugged
+    path: ':rails_root/public/screenshots/:style/:id_sha1.png',
+    url:  "#{ActionController::Base.relative_url_root}/screenshots/:style/:id_sha1.png"
+  friendly_id :image_sha1, use: :slugged
+
+  def image_sha1
+    File.basename(image.url[/(.*)\?.*/, 1], '.png')
+  end
 end
