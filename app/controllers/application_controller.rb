18e8d005fde262b7b8dd8fa5e1542b6b17b54af9
chiastolite <chiastolite.1980@gmail.com>
2014-01-24 22:02:41 +0900

    [from now] 2014/01/24 22:02:41
    diff --git a/app/controllers/application_controller.rb b/app/controllers/application_controller.rb
    index 85c3cd2..042c6d3 100644
    --- a/app/controllers/application_controller.rb
    +++ b/app/controllers/application_controller.rb
    @@ -3,11 +3,23 @@ class ApplicationController < ActionController::Base
       # For APIs, you may want to use :null_session instead.
       protect_from_forgery with: :exception
    
    -  def user_id
    +  def gyazo_id
         params['id'] || new_id
       end
    
       def new_id
         Digest::MD5.hexdigest(request[:remote_addr] + Time.now.to_s)
       end
    +
    +  def new_user?
    +    params[:id].present?
    +  end
    +
    +  def current_user
    +    User.find(gyazo_id: gyazo_id) || User.create(gyazo_id: gyazo_id)
    +  end
    +
    +  def hoge
    +    header['X-Gyazo-Id'] = gyazo_id if new_user?
    +  end
     end

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,23 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def user_id
+  def gyazo_id
     params['id'] || new_id
   end
 
   def new_id
     Digest::MD5.hexdigest(request[:remote_addr] + Time.now.to_s)
+  end
+
+  def new_user?
+    params[:id].present?
+  end
+
+  def current_user
+    User.find(gyazo_id: gyazo_id) || User.create(gyazo_id: gyazo_id)
+  end
+
+  def hoge
+    header['X-Gyazo-Id'] = gyazo_id if new_user?
   end
 end
